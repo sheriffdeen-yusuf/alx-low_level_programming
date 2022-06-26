049f3f57da08c22488c717ad936aad82ecf4a343
sheriffdeen yusuf <sheriffdeenyusuf1130@gmail.com>
2022-06-26 11:52:38 -0700

    edits same

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@
 
 int main(void)
 {
-	putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 	return (1);
 }
