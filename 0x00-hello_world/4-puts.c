74261a807177ad662004fad6225ca2497d13dae5
sheriffdeen yusuf <sheriffdeenyusuf1130@gmail.com>
2022-06-26 01:27:48 -0700

    Added betty code and docs styel

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
-/*
+/**
 * main - Entry point
 *
 *Return: Always 0 (success)
-*puts: Standard output with newlinw
 */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
