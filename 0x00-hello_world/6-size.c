58c1dbba0060d9d75699eadbcd9b0c8c6eb6044c
sheriffdeen yusuf <sheriffdeenyusuf1130@gmail.com>
2022-06-26 10:02:43 -0700

    change textin 6-sie

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,11 +15,10 @@ int main(void)
 	long long lli;
 	float f;
 
-	printf("Size of a char: %d bytes(s)\n", sizeof(c));
-	printf("Size of an int: %d bytes(s)\n", sizeof(i));
-	printf("Size of a long int: %d bytes(s)\n", sizeof(li));
-	printf("Size of a long long int: %d bytes(s)\n", sizeof(lli));
-	printf("Size of a float: %d bytes(s)\n", sizeof(f));
-	
+	printf("Size of a char: %d byte(s)\n", sizeof(c));
+	printf("Size of an int: %d byte(s)\n", sizeof(i));
+	printf("Size of a long int: %d byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(lli));
+	printf("Size of a float: %d byte(s)\n", sizeof(f));
 	return (0);	
 }
