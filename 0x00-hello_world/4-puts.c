15bb72238e41030bfe2f91a1092e5bf85c7a8936
sheriffdeen yusuf <sheriffdeenyusuf1130@gmail.com>
2022-06-26 01:22:53 -0700

    Added betty code and docs style for 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+/*
+* main - Entry point
+*
+*Return: Always 0 (success)
+*puts: Standard output with newlinw
+*/
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }
