74f958e6e019ce305eb2fab3911aedfc4bb8dc76
sheriffdeen yusuf <sheriffdeenyusuf1130@gmail.com>
2022-07-01 07:48:32 -0700

    Added three more tasks

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*
+*Return: Always 0 (success)
+*
+*/
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{	
+		if (ch == 'q' || ch == 'e')
+			continue;
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
