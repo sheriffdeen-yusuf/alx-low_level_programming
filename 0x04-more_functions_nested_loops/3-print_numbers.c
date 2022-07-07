9e4c9e29733f6e41361ca41ca374e738c365f4a8
sheriffdeen yusuf <sheriffdeenyusuf1130@gmail.com>
2022-07-07 10:16:07 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_number - Prints integer from 0 to 9
+*
+* Return: 0 Always true
+*/
+
+void print_number(void)
+{
+	int i;
+
+	while (i < 10)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
