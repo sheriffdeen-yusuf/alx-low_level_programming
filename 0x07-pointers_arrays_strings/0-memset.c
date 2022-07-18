7cc5a47fb6247b1089783308e6bd7001a21fd16f
sheriffdeen yusuf <sheriffdeenyusuf1130@gmail.com>
2022-07-18 11:46:14 -0700

    task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* _memset - Fills memory area with constant
+* @s: array to be filled
+* @b: constant byte
+* @n: length of required memory
+* Returns:  a pointer to the memory area s
+*/
+
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int len = 0;
+
+	while (len < n)
+	{
+		s[len] = b;
+		len++;
+	}
+
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+More on pointers, Arrays and Strings
