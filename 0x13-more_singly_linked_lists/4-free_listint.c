2f198c1a16e868791c69d95f7b0449d470659a1b
sheriffdeen yusuf <sheriffdeenyusuf1130@gmail.com>
2022-08-16 10:09:25 -0700

    complete task 4

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -8,12 +8,12 @@
 
 void free_listint(listint_t *head)
 {
-	listint_t *tmp = head;
+	listint_t *tmp;
 
 	while (head)
 	{
+		tmp = head->next;
 		free(head);
-		tmp = tmp->next;
 		head = tmp;
 	}
 	
